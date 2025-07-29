55fed392a160ba4ca4ef73381e654072058b0373
hangsheng0625 <hangsheng279@gmail.com>
2025-07-29 18:33:41 +0800

    Complete the app.jsx routings and layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ import Layout from './pages/Layout.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import WriteArticle from './pages/WriteArticle.jsx'
 import BlogTitle from './pages/BlogTitle.jsx'
+import GenerateImages from './pages/GenerateImages.jsx'
+import RemoveBackground from './pages/RemoveBackgrounds.jsx'
+import RemoveObject from './pages/RemoveObject.jsx'
+import ReviewResume from './pages/ReviewResume.jsx'
+import Community from './pages/Community.jsx'
 
 const App = () => {
   return (
@@ -14,8 +19,13 @@ const App = () => {
         <Route path='/' element={<Home />}/>
         <Route path='/ai' element={<Layout />}>
           <Route index element={<Dashboard />} />
-          <Route path='write-article' element={<WriteArticle />} />
-          <Route path='blog-title' element={<BlogTitle />} />
+          <Route path='write-articles' element={<WriteArticle />} />
+          <Route path='blog-titles' element={<BlogTitle />} />
+          <Route path='generate-images' element={<GenerateImages />} />
+          <Route path='remove-background' element={<RemoveBackground />} />
+          <Route path='remove-object' element={<RemoveObject />} />
+          <Route path='review-resume' element={<ReviewResume />} />
+          <Route path='community' element={<Community />} />
         </Route>
 
       </Routes>
