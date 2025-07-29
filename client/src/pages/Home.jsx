6b8bfa6fd16604e4666589142abb417460f0023b
hangsheng0625 <hangsheng279@gmail.com>
2025-07-29 19:12:33 +0800

    modify the main.jsx to integrate the clerk for user authentication

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import Hero from '../components/Hero.jsx'
 
 const Home = () => {
   return (
-    <div>
-        <h1>Home</h1>
-    </div>
+    <>
+    <Navbar></Navbar>
+    <Hero />
+    </>
   )
 }
 
